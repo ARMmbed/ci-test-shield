45b127907b343c022bbfe7a5957e49c49308c32b
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-07-11 17:16:06 -0500

    updating I2C Tests

--- TESTS/API/I2C/I2C.cpp
+++ TESTS/API/I2C/I2C.cpp
@@ -13,43 +13,28 @@ using namespace utest::v1;
 const char *convBool(bool value) { return value ? "true" : "false"; }
 
 #define I2C_SENTINAL 'Z'
+#define TMP102_ADDR 0x90
+#define EEPROM_ADDR 0xA0
 
 // a test to see if the temperature can be read. A I2C failure returns a 0
 void test_tmp102(){
-    TMP102 temperature(I2C_SDA, I2C_SCL, 0x90); //A0 pin is connected to ground
+    TMP102 temperature(I2C_SDA, I2C_SCL, TMP102_ADDR); //A0 pin is connected to ground
 	TEST_ASSERT(0 != temperature.read());
 }
 
 // A test to write the EEprom
 void test_eeprom_W(){
-    I2CEeprom memory(I2C_SDA,I2C_SCL, 0xA0, 32, 0);
-	TEST_ASSERT(memory.write(0x100, I2C_SENTINAL) == 1);
+    I2CEeprom memory(I2C_SDA,I2C_SCL, EEPROM_ADDR, 32, 0);
+	TEST_ASSERT(memory.write(1, I2C_SENTINAL) == 1); // check data was written
 }
 
 
 // A test to read the EEprom
 void test_eeprom_R(){
-    I2CEeprom memory(I2C_SDA,I2C_SCL, 0xA0, 32, 0);
+    I2CEeprom memory(I2C_SDA,I2C_SCL, EEPROM_ADDR, 32, 0);
 	char value = 'a';
-    int size = 0;
-    size = memory.read(0x100,value);
-    printf("********\nValue retreived = '%c', size = %d\n********\n",value,size);
-        size = memory.read(0x100,value);
-    printf("********\nValue retreived = '%c', size = %d\n********\n",value,size);
-        size = memory.read(0x100,value);
-    printf("********\nValue retreived = '%c', size = %d\n********\n",value,size);
-        size = memory.read(0x100,value);
-    printf("********\nValue retreived = '%c', size = %d\n********\n",value,size);
-        size = memory.read(0x100,value);
-    printf("********\nValue retreived = '%c', size = %d\n********\n",value,size);
-    TEST_ASSERT(value == I2C_SENTINAL);
-    // if (memory.read(1, value) == 0) {
-    //     printf("******\nRead failed\n*******\n");
-    //     TEST_ASSERT(false);
-    // } else {
-    //     printf("******\nValue read [%c] valid %s\n*******\n", value, convBool(value == 'Z'));
-    //     TEST_ASSERT(true);
-    // }
+    TEST_ASSERT(memory.read(1,value) == 1); // check data was read
+    TEST_ASSERT(I2C_SENTINAL == value); // check data integrity
 }
 
 // A test to Write then read the eeprom
