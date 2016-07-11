76016388eeca6ebbecbeee87d6e14ce67e8ccf8c
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-07-11 18:35:33 -0500

    default SPI tests to failure as they are not fully flushed out yet

--- TESTS/API/SPI/SPI.cpp
+++ TESTS/API/SPI/SPI.cpp
@@ -6,7 +6,7 @@
 #include "rtos.h"
 // #include "SDFileSystem.h"
 
-// using namespace utest::v1;
+ using namespace utest::v1;
 
 // SDFileSystem sd(D11, D12, D13, D10, "sd"); // the pinout on the mbed Cool Components workshop board
 
@@ -14,7 +14,6 @@
 // void test_sd_write(){
 //     mkdir("/sd/mydir", 0777);
 //     FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
-    
 //     TEST_ASSERT(fp != NULL);
 //     // if(fp == NULL) {
 //     //     error("Could not open file for write\n");
@@ -23,20 +22,25 @@
 //     fclose(fp);
 // }
 
-// utest::v1::status_t test_setup(const size_t number_of_cases) {
-//     // Setup Greentea using a reasonable timeout in seconds
-//     GREENTEA_SETUP(40, "default_auto");
-//     return verbose_test_setup_handler(number_of_cases);
-// }
+void test_sd_write(){
+	// Currently no SD Test written, waiting for public release to enable
+	TEST_ASSERT(false);
+}
+
+utest::v1::status_t test_setup(const size_t number_of_cases) {
+    // Setup Greentea using a reasonable timeout in seconds
+    GREENTEA_SETUP(40, "default_auto");
+    return verbose_test_setup_handler(number_of_cases);
+}
 
 // // Test cases
-// Case cases[] = {
-//     Case("Testing SPI via SD Write", test_sd_write),
-// };
+ Case cases[] = {
+     Case("Testing SPI via SD Write", test_sd_write),
+ };
 
-// Specification specification(test_setup, cases);
+ Specification specification(test_setup, cases);
 
 // // Entry point into the tests
 int main() {
-//    return !Harness::run(specification);
+    return !Harness::run(specification);
 }
