ba586b2c0a8ecde65791218445e01c812fc6e360
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-07-12 12:24:06 -0500

    Templated Interrupt In Tests

--- TESTS/API/InterruptIn/InterruptIn.cpp
+++ TESTS/API/InterruptIn/InterruptIn.cpp
@@ -11,13 +11,16 @@ volatile bool result = false;
 // Callback for all InterruptInput functions
 void cbfn(void){
 	result = true;
-	printf("\t**** Interrupt Triggered!\n");
+	//printf("\t**** Interrupt Triggered!\n");
 }
 
-// Test Interrupt Input 
-void test_intin_d0(){
-	InterruptIn intin(D0);
-	DigitalOut dout(D1);
+// Template to check Falling edge and Risign edge interrupts.
+template <PinName int_pin, PinName dout_pin> 
+void InterruptInTest()
+{
+	result = false;
+	InterruptIn intin(int_pin);
+	DigitalOut dout(dout_pin);
 
 	// Test Rising Edge InterruptIn
 	//printf("***** Rising Edge Test \n");
@@ -38,9 +41,36 @@ void test_intin_d0(){
 	wait(1);
 	//printf("Value of result is : %d\n",result);
 	TEST_ASSERT(result);
-
-	//printf("***** Test Finished! \n");
 }
+
+
+// // Test Interrupt Input 
+// void InterruptInTest(){
+// 	InterruptIn intin(D0);
+// 	DigitalOut dout(D1);
+
+// 	// Test Rising Edge InterruptIn
+// 	//printf("***** Rising Edge Test \n");
+// 	dout = 0;
+// 	result = false;
+// 	intin.rise(cbfn);
+// 	dout = 1;
+// 	wait(1);
+// 	//printf("Value of result is : %d\n",result);
+// 	TEST_ASSERT(result);
+
+// 	// Test Falling Edge InterruptIn
+// 	//printf("***** Falling Edge Test \n");
+// 	dout = 1;
+// 	result = false;
+// 	intin.fall(cbfn);
+// 	dout = 0;
+// 	wait(1);
+// 	//printf("Value of result is : %d\n",result);
+// 	TEST_ASSERT(result);
+
+// 	//printf("***** Test Finished! \n");
+// }
 
 utest::v1::status_t test_setup(const size_t number_of_cases) {
     // Setup Greentea using a reasonable timeout in seconds
@@ -50,7 +80,16 @@ utest::v1::status_t test_setup(const size_t number_of_cases) {
 
 // Test cases
 Case cases[] = {
-    Case("Test InterruptIn on D0", test_intin_d0),
+    Case("Test InterruptIn on D0", InterruptInTest<D0,D1>),
+    Case("Test InterruptIn on D1", InterruptInTest<D1,D0>),
+    Case("Test InterruptIn on D2", InterruptInTest<D2,D3>),
+    Case("Test InterruptIn on D3", InterruptInTest<D3,D2>),
+    Case("Test InterruptIn on D4", InterruptInTest<D4,D5>),
+    Case("Test InterruptIn on D5", InterruptInTest<D5,D4>),
+    Case("Test InterruptIn on D6", InterruptInTest<D6,D7>),
+    Case("Test InterruptIn on D7", InterruptInTest<D7,D6>),
+    Case("Test InterruptIn on D8", InterruptInTest<D8,D9>),
+    Case("Test InterruptIn on D9", InterruptInTest<D9,D8>),
 };
 
 Specification specification(test_setup, cases);
