e774cf4106ec7ec6f02248847b9471c40fcc93cb
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-07-12 12:27:34 -0500

    removing old interrupt in tests

--- TESTS/API/InterruptIn/InterruptIn.cpp
+++ TESTS/API/InterruptIn/InterruptIn.cpp
@@ -43,35 +43,6 @@ void InterruptInTest()
 	TEST_ASSERT(result);
 }
 
-
-// // Test Interrupt Input 
-// void InterruptInTest(){
-// 	InterruptIn intin(D0);
-// 	DigitalOut dout(D1);
-
-// 	// Test Rising Edge InterruptIn
-// 	//printf("***** Rising Edge Test \n");
-// 	dout = 0;
-// 	result = false;
-// 	intin.rise(cbfn);
-// 	dout = 1;
-// 	wait(1);
-// 	//printf("Value of result is : %d\n",result);
-// 	TEST_ASSERT(result);
-
-// 	// Test Falling Edge InterruptIn
-// 	//printf("***** Falling Edge Test \n");
-// 	dout = 1;
-// 	result = false;
-// 	intin.fall(cbfn);
-// 	dout = 0;
-// 	wait(1);
-// 	//printf("Value of result is : %d\n",result);
-// 	TEST_ASSERT(result);
-
-// 	//printf("***** Test Finished! \n");
-// }
-
 utest::v1::status_t test_setup(const size_t number_of_cases) {
     // Setup Greentea using a reasonable timeout in seconds
     GREENTEA_SETUP(40, "default_auto");
