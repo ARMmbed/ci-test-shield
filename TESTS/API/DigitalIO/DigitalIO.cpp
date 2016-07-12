da14cbcc71647b0e77de712881769a4b7562a87c
unknown <ausbla01@U201184.usa.Arm.com>
2016-07-12 14:06:17 -0500

    moved Digital IO tests to Template

--- TESTS/API/DigitalIO/DigitalIO.cpp
+++ TESTS/API/DigitalIO/DigitalIO.cpp
@@ -6,11 +6,12 @@
 
 using namespace utest::v1;
 
-
-// Tests that assert are considered failing
-void test_D0() {
-    DigitalOut dout(D0);
-    DigitalIn din(D1);
+// Template to test paired Digital IO pins, meant to be re-used multiple times
+template <PinName dout_pin, PinName din_pin> 
+void DigitalIO_Test()
+{
+    DigitalOut dout(dout_pin);
+    DigitalIn din(din_pin);
     // test 0
     dout = 0;
     TEST_ASSERT(0 == din.read());
@@ -18,115 +19,6 @@ void test_D0() {
     dout = 1;
     TEST_ASSERT(1 == din.read());
 }
-
-// Tests that assert are considered failing
-void test_D1() {
-    DigitalOut dout(D1);
-    DigitalIn din(D0);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D2() {
-    DigitalOut dout(D2);
-    DigitalIn din(D3);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D3() {
-    DigitalOut dout(D3);
-    DigitalIn din(D2);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D4() {
-    DigitalOut dout(D4);
-    DigitalIn din(D5);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D5() {
-    DigitalOut dout(D5);
-    DigitalIn din(D4);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D6() {
-    DigitalOut dout(D6);
-    DigitalIn din(D7);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D7() {
-    DigitalOut dout(D7);
-    DigitalIn din(D6);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D8() {
-    DigitalOut dout(D8);
-    DigitalIn din(D9);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
-// Tests that assert are considered failing
-void test_D9() {
-    DigitalOut dout(D9);
-    DigitalIn din(D8);
-    // test 0
-    dout = 0;
-    TEST_ASSERT(0 == din.read());
-    // test 1
-    dout = 1;
-    TEST_ASSERT(1 == din.read());
-}
-
 
 utest::v1::status_t test_setup(const size_t number_of_cases) {
     // Setup Greentea using a reasonable timeout in seconds
@@ -136,16 +28,16 @@ utest::v1::status_t test_setup(const size_t number_of_cases) {
 
 // Test cases
 Case cases[] = {
-    Case("Testing Digital I/O on D2", test_D2),
-    Case("Testing Digital I/O on D3", test_D3),
-    Case("Testing Digital I/O on D4", test_D4),
-    Case("Testing Digital I/O on D5", test_D5),
-    Case("Testing Digital I/O on D6", test_D6),
-    Case("Testing Digital I/O on D7", test_D7),
-    Case("Testing Digital I/O on D8", test_D8),
-    Case("Testing Digital I/O on D9", test_D9),
-    Case("Testing Digital I/O on D0", test_D0),
-    Case("Testing Digital I/O on D1", test_D1),
+    Case("Testing Digital I/O on D2", DigitalIO_Test<D2,D3>),
+    Case("Testing Digital I/O on D3", DigitalIO_Test<D3,D2>),
+    Case("Testing Digital I/O on D4", DigitalIO_Test<D4,D5>),
+    Case("Testing Digital I/O on D5", DigitalIO_Test<D5,D4>),
+    Case("Testing Digital I/O on D6", DigitalIO_Test<D6,D7>),
+    Case("Testing Digital I/O on D7", DigitalIO_Test<D7,D6>),
+    Case("Testing Digital I/O on D8", DigitalIO_Test<D8,D9>),
+    Case("Testing Digital I/O on D9", DigitalIO_Test<D9,D8>),
+    Case("Testing Digital I/O on D0", DigitalIO_Test<D0,D1>),
+    Case("Testing Digital I/O on D1", DigitalIO_Test<D1,D0>),
 
 };
 
