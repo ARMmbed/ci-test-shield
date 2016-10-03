7657de23910df89b8a7394b17e1a9eaccaeeda88
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-10-03 18:52:29 -0500

    added extra businout tests

--- TESTS/API/BusInOut/BusInOut.cpp
+++ TESTS/API/BusInOut/BusInOut.cpp
@@ -6,7 +6,7 @@
 using namespace utest::v1;
 
 
-// test that all pins can be marked as busin
+// test that all pins can be marked as BusIn
 void busin_define_test(){
     BusIn bin(MBED_CONF_APP_DIO_2,MBED_CONF_APP_DIO_3,MBED_CONF_APP_DIO_4,MBED_CONF_APP_DIO_5,MBED_CONF_APP_DIO_6,MBED_CONF_APP_DIO_7,MBED_CONF_APP_DIO_8,MBED_CONF_APP_DIO_9,MBED_CONF_APP_SPI_CS,MBED_CONF_APP_SPI_MOSI,MBED_CONF_APP_AIN_0,MBED_CONF_APP_AIN_1,MBED_CONF_APP_AIN_2,MBED_CONF_APP_AIN_3,MBED_CONF_APP_AIN_4,MBED_CONF_APP_AIN_5);
     int x = 0;
@@ -14,7 +14,7 @@ void busin_define_test(){
     TEST_ASSERT_MESSAGE(true,"The fact that it hasnt error out proves this passes the sniff test");
 }
 
-// test that all pins can be marked as busin
+// test that all pins can be marked as BusOut
 void busout_define_test(){
     BusOut bout(MBED_CONF_APP_DIO_2,MBED_CONF_APP_DIO_3,MBED_CONF_APP_DIO_4,MBED_CONF_APP_DIO_5,MBED_CONF_APP_DIO_6,MBED_CONF_APP_DIO_7,MBED_CONF_APP_DIO_8,MBED_CONF_APP_DIO_9,MBED_CONF_APP_SPI_CS,MBED_CONF_APP_SPI_MOSI,MBED_CONF_APP_AIN_0,MBED_CONF_APP_AIN_1,MBED_CONF_APP_AIN_2,MBED_CONF_APP_AIN_3,MBED_CONF_APP_AIN_4,MBED_CONF_APP_AIN_5);
     bout = 0;
@@ -23,6 +23,41 @@ void busout_define_test(){
         //printf("\r\n*********\r\nvalue of x is: 0x%x\r\n********\r\n",x);
         x++;
         bout.write(x);
+    }
+    TEST_ASSERT_MESSAGE(true,"The fact that it hasnt error out proves this passes the sniff test");
+}
+
+// // test that all pins can be marked as BusInOut
+// void businout_define_test(){
+//     BusInOut bio(MBED_CONF_APP_DIO_2,MBED_CONF_APP_DIO_3,MBED_CONF_APP_DIO_4,MBED_CONF_APP_DIO_5,MBED_CONF_APP_DIO_6,MBED_CONF_APP_DIO_7,MBED_CONF_APP_DIO_8,MBED_CONF_APP_DIO_9,MBED_CONF_APP_SPI_CS,MBED_CONF_APP_SPI_MOSI,MBED_CONF_APP_AIN_0,MBED_CONF_APP_AIN_1,MBED_CONF_APP_AIN_2,MBED_CONF_APP_AIN_3,MBED_CONF_APP_AIN_4,MBED_CONF_APP_AIN_5);
+//     bio.output();
+//     bio = 0x00;
+//     volatile int x = 0x00;
+//     while(x < 0xFF){
+//         x = (x<<1) +1;
+//         bio.output();
+//         bio = x;
+//         wait(1);
+//         bio.input();
+//         wait(1);
+//         volatile int y = bio.read();
+//         //printf("\r\n*********\r\nvalue of x,bio is: 0x%x, 0x%x\r\n********\r\n",x,y);
+//         TEST_ASSERT_MESSAGE(y == x,"Value read on bus does not equal value written. ");
+//     }
+//     TEST_ASSERT_MESSAGE(true,"The fact that it hasnt error out proves this passes the sniff test");
+// }
+
+// Test writing from one bus to another
+void businout_bidirectional_test(){
+    BusIn bin(MBED_CONF_APP_DIO_2,MBED_CONF_APP_DIO_4,MBED_CONF_APP_DIO_6,MBED_CONF_APP_DIO_8);
+    BusOut bout(MBED_CONF_APP_DIO_3,MBED_CONF_APP_DIO_5,MBED_CONF_APP_DIO_7,MBED_CONF_APP_DIO_9);
+    bout = 0;
+    volatile int x = 0;
+    while(x < 0x0F){
+        x++;
+        bout.write(x);
+        //printf("\r\n*********\r\nvalue of bin,bout,x is: 0x%x, 0x%x, 0x%x\r\n********\r\n",bin.read(),bout.read(),x);
+        TEST_ASSERT_MESSAGE(bin.read() == bout.read(),"Value read on bin does not equal value written on bout. ")
     }
     TEST_ASSERT_MESSAGE(true,"The fact that it hasnt error out proves this passes the sniff test");
 }
@@ -44,6 +79,8 @@ utest::v1::status_t greentea_failure_handler(const Case *const source, const fai
 Case cases[] = {
     Case("Test BusIn definable", busin_define_test,greentea_failure_handler),
     Case("Test BusOut definable", busout_define_test,greentea_failure_handler),
+    // Case("Test BusInOut definable", businout_define_test,greentea_failure_handler),
+    Case("Test BusIn to BusOut test", businout_bidirectional_test,greentea_failure_handler),
 };
 
 Specification specification(test_setup, cases);
