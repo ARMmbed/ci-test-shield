0a12702fb5ff0d3683b2d532ceeaced6652e89f5
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-10-04 01:20:10 -0500

    updated I2C test, moved peripheral addresses to config file

--- TESTS/API/I2C/I2C.cpp
+++ TESTS/API/I2C/I2C.cpp
@@ -13,17 +13,13 @@
 
 using namespace utest::v1;
 
-// TODO: take both A0 and 0x90 and put into config file
-
 const char *convBool(bool value) { return value ? "true" : "false"; }
 
 #define I2C_SENTINAL 'Z' //TODO: make this a random letter or string
-#define TMP102_ADDR 0x90
-#define EEPROM_ADDR 0xA0
 
 // a test to see if the temperature can be read. A I2C failure returns a 0
 void test_tmp102(){
-    //TMP102 temperature(MBED_CONF_APP_I2C_SDA, MBED_CONF_APP_I2C_SCL, TMP102_ADDR); //A0 pin is connected to ground
+    //TMP102 temperature(MBED_CONF_APP_I2C_SDA, MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_TEMP_ADDR); //A0 pin is connected to ground
     LM75B  temperature(MBED_CONF_APP_I2C_SDA, MBED_CONF_APP_I2C_SCL);
 	TEST_ASSERT_MESSAGE(0 != temperature.open(),"Failed to open sensor");
     TEST_ASSERT_MESSAGE(NULL != temperature.temp(),"Invalid value NULL returned");
@@ -33,14 +29,14 @@ void test_tmp102(){
 
 // A test to write the EEprom
 void test_eeprom_W(){
-    I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, EEPROM_ADDR, 32, 0);
+    I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_EEPROM_ADDR, 32, 0);
 	TEST_ASSERT_MESSAGE(memory.write(1, I2C_SENTINAL) == 1,"data not written, error with EEPROM?"); // check data was written
 }
 
 
 // A test to read the EEprom
 void test_eeprom_R(){
-    I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, EEPROM_ADDR, 32, 0);
+    I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_EEPROM_ADDR, 32, 0);
 	char value = 'a';
     TEST_ASSERT_MESSAGE(memory.read(1,value) == 1,"data failed to be read correctly"); // check data was read
     TEST_ASSERT_MESSAGE(I2C_SENTINAL == value,"data read does not match data written! bad EEPROM?"); // check data integrity
