00bde670e20a31ab5657832c311425bcdcda6f2c
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-10-04 01:04:29 -0500

    added default pwm test sanity check

--- TESTS/API/Pwm/Pwm.cpp
+++ TESTS/API/Pwm/Pwm.cpp
@@ -1,7 +1,7 @@
-#error [NOT_SUPPORTED] PWM tests are still being written and validated, not for public use yet. 
+//#error [NOT_SUPPORTED] PWM tests are still being written and validated, not for public use yet. 
 
 #if !DEVICE_PWM
-  #error PWM not supported on this platform, add 'DEVICE_ANALOGIN' deffinition to your platform.
+  #error PWM not supported on this platform, add 'DEVICE_PWM' deffinition to your platform.
 #endif
 
 #include "mbed.h"
@@ -16,20 +16,19 @@ using namespace utest::v1;
 #define MIN_DUTY_CYCLE   0.1
 #define MAX_DUTY_CYCLE   0.9
 
-volatile rise_count;
+volatile int rise_count;
 void cbfn_rise(void){
     rise_count++;
 }
 
-volatile fall_count; 
+volatile int fall_count; 
 void cbfn_fall(void){
     fall_count++;
 }
 
 // Template to set one Analog pin as input and then cycle through the rest as outputs.
 // As you turn more pins on the voltage on the ain pin will go up.
-template <PinName pwm_out_pin, PinName int_in_pin, float period_in_seconds, float duty_cycle_percent> 
-void PWM_Test_slave()
+void PWM_Test_slave(PinName pwm_out_pin, PinName int_in_pin, int period_in_seconds, int duty_cycle_percent)
 {
     PwmOut pwm(pwm_out_pin);
     InterruptIn iin(int_in_pin);
@@ -42,13 +41,33 @@ void PWM_Test_slave()
 }
 
 // Template to itterate through a PWM pin, takes in period and tries 10%-100% duty cycle in intervals of 10%
-template <PinName pwm_out_pin, PinName int_in_pin, float period_in_seconds> 
+template <PinName pwm_out_pin, PinName int_in_pin, int period_in_seconds> 
 void PWM_Test()
 {
     int x = 0;
-    for(x = MIN_DUTY_CYCLE; x < MAX_DUTY_CYCLE, x = x+DUTY_CYCLE_STEP){ // itterate duty cycle test
+    for(x = MIN_DUTY_CYCLE; x < MAX_DUTY_CYCLE; x = x+DUTY_CYCLE_STEP){ // itterate duty cycle test
         PWM_Test_slave(pwm_out_pin, int_in_pin, period_in_seconds, x);
     }
+}
+
+// test if software constructor / destructor works
+void pwm_define_test(){
+    PwmOut pwm0(MBED_CONF_APP_PWM_0);
+    PwmOut pwm1(MBED_CONF_APP_PWM_1);
+    PwmOut pwm2(MBED_CONF_APP_PWM_2);
+    PwmOut pwm3(MBED_CONF_APP_PWM_3);
+
+    pwm0.period(1.0f);
+    pwm1.period(1.0f);
+    pwm2.period(1.0f);
+    pwm3.period(1.0f);
+
+    pwm0.write(0.5f);
+    pwm1.write(0.5f);
+    pwm2.write(0.5f);
+    pwm3.write(0.5f);
+
+    TEST_ASSERT_MESSAGE(true,"The fact that it hasnt errored out proves this passes the sniff test");
 }
 
 utest::v1::status_t test_setup(const size_t number_of_cases) {
@@ -66,10 +85,11 @@ utest::v1::status_t greentea_failure_handler(const Case *const source, const fai
 // Test cases
 // TODO: take pin names from config file or generate from pinmap file
 Case cases[] = {
-    Case("Test PWM on PWM_0", PWM_Test<MBED_CONF_APP_PWM_0,MBED_CONF_APP_DIO_2,4.0f>,greentea_failure_handler), // 4second period, 50% duty cycle
-    Case("Test PWM on PWM_1", PWM_Test<MBED_CONF_APP_PWM_1,MBED_CONF_APP_DIO_4,4.0f>,greentea_failure_handler), // 4second period, 50% duty cycle
-    Case("Test PWM on PWM_2", PWM_Test<MBED_CONF_APP_PWM_2,MBED_CONF_APP_DIO_7,4.0f>,greentea_failure_handler), // 4second period, 50% duty cycle
-    Case("Test PWM on PWM_3", PWM_Test<MBED_CONF_APP_PWM_3,MBED_CONF_APP_DIO_8,4.0f>,greentea_failure_handler)  // 4second period, 50% duty cycle
+    Case("Test Pwm definable", pwm_define_test,greentea_failure_handler),                                        // test pwm object contructor works
+    //Case("Test PWM on PWM_0", PWM_Test<MBED_CONF_APP_PWM_0,MBED_CONF_APP_DIO_2,4>,greentea_failure_handler), // 4second period, 50% duty cycle
+    //Case("Test PWM on PWM_1", PWM_Test<MBED_CONF_APP_PWM_1,MBED_CONF_APP_DIO_4,4.0f>,greentea_failure_handler), // 4second period, 50% duty cycle
+    //Case("Test PWM on PWM_2", PWM_Test<MBED_CONF_APP_PWM_2,MBED_CONF_APP_DIO_7,4.0f>,greentea_failure_handler), // 4second period, 50% duty cycle
+    //Case("Test PWM on PWM_3", PWM_Test<MBED_CONF_APP_PWM_3,MBED_CONF_APP_DIO_8,4.0f>,greentea_failure_handler)  // 4second period, 50% duty cycle
 };
 
 Specification specification(test_setup, cases);
