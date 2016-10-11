f6d98eb829bba3e0303a82a840692f0193602fc9
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-10-11 15:50:44 -0500

    added I2C tests

--- TESTS/API/I2C/I2C.cpp
+++ TESTS/API/I2C/I2C.cpp
@@ -7,22 +7,33 @@
 #include "greentea-client/test_env.h"
 #include "unity.h"
 #include "utest.h"
-//#include "rtos.h"
 #include "LM75B.h"
 #include <I2CEeprom.h>
 
 using namespace utest::v1;
 
-const char *convBool(bool value) { return value ? "true" : "false"; }
 
-#define I2C_SENTINAL 'Z' //TODO: make this a random letter or string
+#define I2C_TEST_STRING_MAX 100
+int addr = 100;
+char I2C_TEST_STRING[I2C_TEST_STRING_MAX] = {0};
+void init_string(){
+    int x = 0;
+    for(x = 0; x < I2C_TEST_STRING_MAX-1; x++){
+        I2C_TEST_STRING[x] = 'A' + (rand() % 26);
+    }
+    I2C_TEST_STRING[I2C_TEST_STRING_MAX] = 0; // add \0 to end of string
+    addr = rand()% 100; // randomize the start address
+
+    printf("\r\n****\r\nI2C Test addr = %d, String = %s\r\n****\r\n",addr,I2C_TEST_STRING);
+}
 
 // a test to see if the temperature can be read. A I2C failure returns a 0
-void test_tmp102(){
-    //TMP102 temperature(MBED_CONF_APP_I2C_SDA, MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_TEMP_ADDR); //A0 pin is connected to ground
+void test_lm75b(){
     LM75B  temperature(MBED_CONF_APP_I2C_SDA, MBED_CONF_APP_I2C_SCL);
-	TEST_ASSERT_MESSAGE(0 != temperature.open(),"Failed to open sensor");
-    TEST_ASSERT_MESSAGE(NULL != temperature.temp(),"Invalid value NULL returned");
+    int temp = temperature.temp();
+    printf("\r\n****\r\nTemperature Read = `%f`\r\n****\r\n",temp);
+    TEST_ASSERT_MESSAGE(0 != temperature.open(),"Failed to open sensor");
+    //TEST_ASSERT_MESSAGE(NULL != temperature.temp(),"Invalid value NULL returned");
     TEST_ASSERT_MESSAGE(50 > temperature.temp(),"Its too Hot (>10C), Faulty Sensor?");
     TEST_ASSERT_MESSAGE(0 < temperature.temp(),"Its Too Cold (<0C), Faulty Sensor?");
 }
@@ -30,22 +41,42 @@ void test_tmp102(){
 // A test to write the EEprom
 void test_eeprom_W(){
     I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_EEPROM_ADDR, 32, 0);
-	TEST_ASSERT_MESSAGE(memory.write(1, I2C_SENTINAL) == 1,"data not written, error with EEPROM?"); // check data was written
+    int num_bytes = 0;
+    num_bytes = memory.write(addr,I2C_TEST_STRING, I2C_TEST_STRING_MAX);
+    printf("\r\n****\r\nNum bytes written = `%d`\r\n****\r\n",num_bytes);
+	TEST_ASSERT_EQUAL_MESSAGE(num_bytes, I2C_TEST_STRING_MAX,"data not written, error with EEPROM?"); // check data was written
 }
 
 
 // A test to read the EEprom
 void test_eeprom_R(){
     I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_EEPROM_ADDR, 32, 0);
-	char value = 'a';
-    TEST_ASSERT_MESSAGE(memory.read(1,value) == 1,"data failed to be read correctly"); // check data was read
-    TEST_ASSERT_MESSAGE(I2C_SENTINAL == value,"data read does not match data written! bad EEPROM?"); // check data integrity
+	volatile char read_string[I2C_TEST_STRING_MAX] = {0};
+    int read_bytes = 0;
+    read_bytes = memory.read(addr,(char*)read_string,I2C_TEST_STRING_MAX);
+
+    printf("\r\n****\r\nNum bytes read = `%d`, string read = `%s`\r\n****\r\n",read_bytes,read_string);
+    TEST_ASSERT_EQUAL_MESSAGE(read_bytes, I2C_TEST_STRING_MAX,"Failed to read the correct number of bytes.");
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(I2C_TEST_STRING,read_string,"data read does not match data written! bad EEPROM?"); // check data integrity
 }
 
 // A test to Write then read the eeprom
-// void test_eeprom_WR(){
+ void test_eeprom_WR(){
+    int address = rand()%250000;
+    I2CEeprom memory(MBED_CONF_APP_I2C_SDA,MBED_CONF_APP_I2C_SCL, MBED_CONF_APP_I2C_EEPROM_ADDR, 32, 0);
+    int read_bytes = 0;
+    int write_bytes = 0;
+    volatile char read_string[I2C_TEST_STRING_MAX] = {0};
 
-// }
+    write_bytes = memory.write(address,I2C_TEST_STRING,I2C_TEST_STRING_MAX);
+    read_bytes  = memory.read(address,(char *)read_string,I2C_TEST_STRING_MAX);
+
+    printf("\r\n*****\r\nBytes Written = `%d`\r\nBytes Read = `%d` \r\nString Written = `%s`\r\nString Read = `%s`\r\n*****\r\n",write_bytes,read_bytes,I2C_TEST_STRING,read_string);
+
+    TEST_ASSERT_MESSAGE(write_bytes == read_bytes,"Number of bytes written does not match number of bytes read");
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(I2C_TEST_STRING,read_string,"String read does not match string written");
+
+ }
 
 utest::v1::status_t test_setup(const size_t number_of_cases) {
     // Setup Greentea using a reasonable timeout in seconds
@@ -61,15 +92,16 @@ utest::v1::status_t greentea_failure_handler(const Case *const source, const fai
 
 // Test cases
 Case cases[] = {
-    Case("I2C -  TMP102 Temperature Read",test_tmp102,greentea_failure_handler),
+    Case("I2C -  LM75B Temperature Read",test_lm75b,greentea_failure_handler),
 	Case("I2C -  EEProm Write",test_eeprom_W,greentea_failure_handler),
 	Case("I2C -  EEProm Read",test_eeprom_R,greentea_failure_handler),
-//	Case("I2C -  EEProm WR",test_eeprom_WR,greentea_failure_handler),
+	Case("I2C -  EEProm WR",test_eeprom_WR,greentea_failure_handler),
 };
 
 Specification specification(test_setup, cases);
 
 // Entry point into the tests
 int main() {
+    init_string();
     return !Harness::run(specification);
 }
