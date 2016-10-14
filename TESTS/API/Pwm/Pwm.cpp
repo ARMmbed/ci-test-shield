66e49d89fff8ff9b2b9ec1dd10559624644adc42
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-10-14 22:20:26 +0100

    added [NOT_SUPPORTED] to error message on mbed support

--- TESTS/API/Pwm/Pwm.cpp
+++ TESTS/API/Pwm/Pwm.cpp
@@ -1,7 +1,7 @@
 //#error [NOT_SUPPORTED] PWM tests are still being written and validated, not for public use yet. 
 
 #if !DEVICE_PWMOUT
-  #error PWMOUT not supported on this platform, add 'DEVICE_PWMOUT' deffinition to your platform.
+  #error [NOT_SUPPORTED] PWMOUT not supported on this platform, add 'DEVICE_PWMOUT' deffinition to your platform.
 #endif
 
 #include "mbed.h"
