a540e02b9a47ac3322d87cf45285c72dcf90a1e9
Michael Bartling <michael.bartling15@gmail.com>
2017-01-31 11:57:52 -0600

    Update DigitalIO.cpp

--- TESTS/assumptions/DigitalIO/DigitalIO.cpp
+++ TESTS/assumptions/DigitalIO/DigitalIO.cpp
@@ -9,14 +9,14 @@ using namespace utest::v1;
 template <PinName d_pin>
 void test_DigitalIO_NC()
 {
-	TEST_ASSERT_MESSAGE(d_pin != NC, "Pin is NC");
+    TEST_ASSERT_MESSAGE(d_pin != NC, "Pin is NC");
 }
 
 utest::v1::status_t test_setup(const size_t number_of_cases)
 {
-	// Setup Greentea using a reasonable timeout in seconds
-	GREENTEA_SETUP(5, "default_auto");
-	return verbose_test_setup_handler(number_of_cases);
+    // Setup Greentea using a reasonable timeout in seconds
+    GREENTEA_SETUP(5, "default_auto");
+    return verbose_test_setup_handler(number_of_cases);
 }
 
 utest::v1::status_t greentea_failure_handler(const Case* const source, const failure_t reason)
@@ -26,21 +26,21 @@ utest::v1::status_t greentea_failure_handler(const Case* const source, const fai
 }
 
 Case cases[] = {
-	Case("DigitalIO - is pin 0 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_0>, greentea_failure_handler),
-	Case("DigitalIO - is pin 1 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_1>, greentea_failure_handler),
-	Case("DigitalIO - is pin 2 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_2>, greentea_failure_handler),
-	Case("DigitalIO - is pin 3 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_3>, greentea_failure_handler),
-	Case("DigitalIO - is pin 4 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_4>, greentea_failure_handler),
-	Case("DigitalIO - is pin 5 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_5>, greentea_failure_handler),
-	Case("DigitalIO - is pin 6 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_6>, greentea_failure_handler),
-	Case("DigitalIO - is pin 7 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_7>, greentea_failure_handler),
-	Case("DigitalIO - is pin 8 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_8>, greentea_failure_handler),
-	Case("DigitalIO - is pin 9 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_9>, greentea_failure_handler),
+    Case("DigitalIO - is pin 0 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_0>, greentea_failure_handler),
+    Case("DigitalIO - is pin 1 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_1>, greentea_failure_handler),
+    Case("DigitalIO - is pin 2 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_2>, greentea_failure_handler),
+    Case("DigitalIO - is pin 3 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_3>, greentea_failure_handler),
+    Case("DigitalIO - is pin 4 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_4>, greentea_failure_handler),
+    Case("DigitalIO - is pin 5 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_5>, greentea_failure_handler),
+    Case("DigitalIO - is pin 6 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_6>, greentea_failure_handler),
+    Case("DigitalIO - is pin 7 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_7>, greentea_failure_handler),
+    Case("DigitalIO - is pin 8 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_8>, greentea_failure_handler),
+    Case("DigitalIO - is pin 9 connected?", test_DigitalIO_NC<MBED_CONF_APP_DIO_9>, greentea_failure_handler),
 
 };
 
 Specification specification(test_setup, cases);
 
 int main() {
-	return !Harness::run(specification);
+    return !Harness::run(specification);
 }
