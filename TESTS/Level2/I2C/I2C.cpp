4260250f2ee470617fd7f9e58c4ad75a43d08c84
brandonboesch <Brandon.Boesch@arm.com>
2017-08-17 12:08:32 -0500

    Finished testing level2/i2c. The logic used to test if write and read occured was not accurate, and not really necessary for the test to begin with, so was removed.

--- TESTS/Level2/I2C/I2C.cpp
+++ TESTS/Level2/I2C/I2C.cpp
@@ -41,46 +41,42 @@ std::vector<unsigned int> TestFramework::pin_iterators(TS_NC);
 TestFramework test_framework;
 char NULL_STR[128] = {0};
 
-template <int iterations>
+template <int num_bytes>
 utest::v1::control_t test_level2_i2c(const size_t call_count) 
 {
-	// Check to see if the CI test shield I2C pins are connected to the board
+	// Assign I2C pins
 	PinName pin_scl = MBED_CONF_APP_I2C_SCL;
 	PinName pin_sda = MBED_CONF_APP_I2C_SDA;
-	if (TestFramework::find_pin(pin_scl, TestFramework::I2C_SCL)==-1 ||	TestFramework::find_pin(pin_sda, TestFramework::I2C_SDA)==-1) {
-		return utest::v1::CaseNext;
-	}
 
-	DEBUG_PRINTF("Running I2C constructor on SDA pin %d and SCL pin %d\n", pin_sda, pin_scl);
+    // Check to see if the CI test shield I2C pins are connected to the board
+	if (TestFramework::find_pin(pin_scl, TestFramework::I2C_SCL)==-1 ||	
+        TestFramework::find_pin(pin_sda, TestFramework::I2C_SDA)==-1) {
+		TEST_ASSERT_MESSAGE(false,"CI test shield is not properly connected to default I2C pins, or pins are already assigned as greentea's UART pins.");
+    }
+
     TEST_ASSERT_MESSAGE(pin_sda != NC, "SDA Pin is NC");
     TEST_ASSERT_MESSAGE(pin_scl != NC, "SCL Pin is NC");
 
-    // Get a random seed from the Greentea host test
-	srand(TestFramework::get_seed());
-
+    DEBUG_PRINTF("Running I2C constructor on SDA pin %#x and SCL pin %#x\n", pin_sda, pin_scl);
     I2CEeprom memory(pin_sda,pin_scl,MBED_CONF_APP_I2C_EEPROM_ADDR,32,0);
+
+    // Get a random seed from the Greentea host test
+	//srand(TestFramework::get_seed());  // TODO not working properly.
 
 	// Generate a random string
     char test_string[128] = {0};
-    for (int i=0; i<iterations; i++){ 
+    for (int i=0; i<num_bytes; i++){ 
   	  test_string[i] = 'A' + rand()%26;
     }
+
+    // Generate a random starting location for storing string
     int location = rand() % 200 + 1;
 
-    char initial_read[128] = {0};
-    char should_be_null[128] = {0};
+    // Test write/read of random string to random location
     char read_data[128] = {0};
-
-    int initial_read_bytes = memory.read(location, initial_read, iterations);
-    int initial_write_bytes = memory.write(location, NULL_STR, iterations);
-    int secondary_read_bytes = memory.read(location, should_be_null, iterations);
-
-    int w = memory.write(location, test_string, iterations);
-    int r = memory.read(location, read_data, iterations);
-
-    DEBUG_PRINTF("Wrote %d bytes: %s\nRead %d bytes: %s\n", w, test_string, r, read_data);
-    TEST_ASSERT_MESSAGE(strcmp(initial_read,should_be_null) != 0,"Write never occurred. Null characters never got written");
-    TEST_ASSERT_MESSAGE(strcmp(NULL_STR,should_be_null) == 0,"Read is not working. Write occurred yet read grabbed different text");
+    int w = memory.write(location, test_string, num_bytes);
+    int r = memory.read(location, read_data, num_bytes);
+    DEBUG_PRINTF("Wrote %d bytes: %s\nRead %d bytes: %s\nLocation: %d\n", w, test_string, r, read_data, location);
     TEST_ASSERT_MESSAGE(strcmp(test_string,read_data) == 0,"Read and write data does not match");
 
     return utest::v1::CaseNext;
@@ -88,9 +84,9 @@ utest::v1::control_t test_level2_i2c(const size_t call_count)
 
 
 Case cases[] = {
-	Case("Level 2 - I2C test - 1 byte (all pin set)", test_level2_i2c<1>, TestFramework::greentea_failure_handler),
-	Case("Level 2 - I2C test - 10 byte (all pin set)", test_level2_i2c<10>, TestFramework::greentea_failure_handler),
-	Case("Level 2 - I2C test - 100 byte (all pin set)", test_level2_i2c<100>, TestFramework::greentea_failure_handler),
+	Case("Level 2 - I2C test - 1 byte to random location", test_level2_i2c<1>, TestFramework::greentea_failure_handler),
+	Case("Level 2 - I2C test - 10 bytes to random location", test_level2_i2c<10>, TestFramework::greentea_failure_handler),
+	Case("Level 2 - I2C test - 100 bytes to random location", test_level2_i2c<100>, TestFramework::greentea_failure_handler),
 };
 
 
